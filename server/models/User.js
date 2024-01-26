efab70c40661e4bf325c30590210d76e6d408829
Uzer Qureshi <uq0803@gmail.com>
2024-01-26 15:37:10 +0530

    feat: added model userSchema

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName:
+    {
+        type:"String",
+        required:true
+    },
+    password:
+    {
+        type:"String",
+        required:true
+    },
+    email:
+    {
+        type:"String",
+        required:true
+    }
+})
+
+export const User = mongoose.model('user' , userSchema)
