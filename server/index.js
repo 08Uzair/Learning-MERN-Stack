5036b1884155ebefb003221bb5ec6704017280b7
Uzer Qureshi <uq0803@gmail.com>
2024-01-26 14:31:17 +0530

    feat:setup done for  cors and bodyParser

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import { connectDatabase } from "./db/connection.js";
 const app = express();
 const portNumber = 8800;
 
+app.use(cors());
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 connectDatabase();
 app.listen(portNumber,()=>{
     console.log(`Server is Running on PORT ${portNumber}`)
